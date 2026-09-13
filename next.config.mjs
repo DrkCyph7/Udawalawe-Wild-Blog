512ecfb9c867bd05afdbcdd6cb7f77fe3811a294
Dinuka Lakshan <74536249+DrkCyph7@users.noreply.github.com>
2026-09-13 23:12:36 +0530

    up

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,16 @@ const nextConfig = {
     unoptimized: true,
   },
   allowedDevOrigins: ['192.168.1.142'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog',
+        basePath: false,
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
